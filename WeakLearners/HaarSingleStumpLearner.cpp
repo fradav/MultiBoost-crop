e7a197b414da4fdfa9e1b271532de9d8d066ead0
busarobi <busarobi@cb4d1683-fb75-0410-9410-9bd0bc004689>
2011-02-03 16:45:44 +0000

    git-svn-id: svn+ssh://mac-88159.lal.in2p3.fr/Users/kegl/Research/SVN/Research/MultiBoost@112 cb4d1683-fb75-0410-9410-9bd0bc004689

--- WeakLearners/HaarSingleStumpLearner.cpp
+++ WeakLearners/HaarSingleStumpLearner.cpp
@@ -45,8 +45,8 @@
 
 namespace MultiBoost {
 	
-	REGISTER_LEARNER_NAME(HaarSingleStump, HaarSingleStumpLearner)
-	
+	//REGISTER_LEARNER_NAME(HaarSingleStumpLearner, HaarSingleStumpLearner)
+	REGISTER_LEARNER(HaarSingleStumpLearner)
 	// ------------------------------------------------------------------------------
 	
 	void HaarSingleStumpLearner::declareArguments(nor_utils::Args& args)
